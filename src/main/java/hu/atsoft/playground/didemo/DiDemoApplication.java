412889329e7378f4b431254e9f5533d70b53e90b
dp <hudomokp@gmail.com>
2018-11-04 21:08:05 +0100

    Initial commit

---
+++ src/main/java/hu/atsoft/playground/didemo/DiDemoApplication.java
@@ -0,0 +1,12 @@
+package hu.atsoft.playground.didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiDemoApplication.class, args);
+	}
+}
