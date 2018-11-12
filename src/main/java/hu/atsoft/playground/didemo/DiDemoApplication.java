78d9600fe3861f7d04241d9f1bc7979930930752
dp <hudomokp@gmail.com>
2018-11-12 18:05:08 +0100

    Added MyController, acquired context, bean from it and invoked hello method.

--- src/main/java/hu/atsoft/playground/didemo/DiDemoApplication.java
+++ src/main/java/hu/atsoft/playground/didemo/DiDemoApplication.java
@@ -2,11 +2,17 @@ package hu.atsoft.playground.didemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import hu.atsoft.playground.didemo.controllers.MyController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+		
+		MyController controller = (MyController) ctx.getBean("myController");
+		controller.hello();
 	}
 }
