0f48a8b345adf5cb47addc440e5998c8e2d3bf57
dp <hudomokp@gmail.com>
2019-02-06 20:56:49 +0100

    Added primary service.

--- src/main/java/hu/atsoft/playground/didemo/controllers/MyController.java
+++ src/main/java/hu/atsoft/playground/didemo/controllers/MyController.java
@@ -2,11 +2,19 @@ package hu.atsoft.playground.didemo.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import hu.atsoft.playground.didemo.services.GreetingService;
+
 @Controller
 public class MyController {
+	
+	private GreetingService greetingService;
+	
+	public MyController(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
 
 	public String hello() {
 		System.out.println("Hello from MyController!!!");
-		return "Hello from MyController!!!";
+		return "Hello from MyController!!!" + " " + greetingService.sayHello();
 	}
 }
